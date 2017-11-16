a635e2ab3c49250c81dc80f854672a7c1bb8aed5
Klaus Kämpf <kkaempf@suse.de>
2017-11-16 10:15:39 +0100

    covert: capture .js files

--- lib/kitbuilder/convert.rb
+++ lib/kitbuilder/convert.rb
@@ -12,23 +12,36 @@ module Kitbuilder
     def convert_to mavendir
       @mavendir = mavendir
       Find.find(@gradledir) do |path|
-        next unless path =~ /\.pom$/
-        pom = Pom.new path
+        dest = nil
         jars = []
-        dir = File.dirname(File.dirname(path))
-        puts "#{dir}:#{pom.basename}"
-        # now look for jars        
-        Find.find(dir) do |local|
-          next unless local =~ /\.jar$/
-          jars << local
-        end
-        dest = File.join(mavendir, pom.dirname)
-        FileUtils.mkdir_p dest
-        FileUtils.cp path, File.join(dest, "#{pom.basename}.pom")
-        jars.each do |jar|
-          name = File.basename(jar)
-          puts "Dir #{dir}, jar #{jar}, name #{name}"
-          FileUtils.cp jar, File.join(dest, name)
+        case path
+        when /\.pom$/
+          pom = Pom.new path
+          dir = File.dirname(File.dirname(path))
+#          puts "#{dir}:#{pom.basename}"
+          # now look for jars        
+          Find.find(dir) do |local|
+            next unless local =~ /\.jar$/
+            jars << local
+          end
+          dest = File.join(mavendir, pom.dirname)
+          FileUtils.mkdir_p dest
+          FileUtils.cp path, File.join(dest, "#{pom.basename}.pom")
+          jars.each do |jar|
+            name = File.basename(jar)
+#            puts "Dir #{dir}, jar #{jar}, name #{name}"
+            FileUtils.cp jar, File.join(dest, name)
+          end
+        when /\.js$/
+#          puts path
+          # ... gradle/caches/modules-2/files-2.1/jquery/jquery.min/1.8.0/f3a55f44fb81cf8ee908a3872841f70d6548f8c1/jquery.min-1.8.0.js
+          dirs = path.split "/"
+          js = dirs.pop
+          dirs.pop # drop checksum
+          dest = File.join(mavendir, dirs.pop(3).join("/"))
+#          puts "dest #{dest}, js #{js}"
+          FileUtils.mkdir_p dest
+          FileUtils.cp path, File.join(dest, js)
         end
       end
     end
