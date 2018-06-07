d12fcd283ff809f8de59fe0b9f83834768c61c65
Klaus Kämpf <kkaempf@suse.de>
2018-06-07 17:05:34 +0200

    look for .zip

--- lib/kitbuilder.rb
+++ lib/kitbuilder.rb
@@ -89,7 +89,7 @@ module Kitbuilder
       script.puts "pushd #{dir}"
       res.each do |k,v|
         next if k == :uri
-        script.print "# " unless [:jar, :jarsha1, :pom, :pomsha1, :signature, :noaop].include? k
+        script.print "# " unless [:jar, :jarsha1, :pom, :pomsha1, :zip, :signature, :noaop].include? k
         script.puts "wget -q #{uri}/#{v}"
       end
       script.puts "popd"
