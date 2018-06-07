47019ee4fd6e046bee1f733b63205313700b9941
Klaus Kämpf <kkaempf@suse.de>
2018-06-07 15:14:27 +0200

    fix jboss

--- lib/kitbuilder/repository/jboss.rb
+++ lib/kitbuilder/repository/jboss.rb
@@ -5,7 +5,7 @@
 module Kitbuilder
   class JBoss < Repository
     def self.build_uri dependency
-      uri = "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases" + dependency.group.split(".").join("/") + "/" + dependency.artifact
+      uri = "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases/" + dependency.group.split(".").join("/") + "/" + dependency.artifact
       if dependency.version
         uri += "/" + dependency.version
       end
