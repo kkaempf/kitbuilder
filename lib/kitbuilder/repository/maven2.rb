2de4c697901b3ea74b54804265fea43abf03ff4e
Klaus Kämpf <kkaempf@suse.de>
2020-03-12 08:07:41 +0100

    repo1.maven.org is https only meanwhile

--- lib/kitbuilder/repository/maven2.rb
+++ lib/kitbuilder/repository/maven2.rb
@@ -5,7 +5,7 @@
 module Kitbuilder
   class Maven2 < Repository
     def self.build_uri dependency
-      uri = "http://repo1.maven.org/maven2/" + dependency.group.split(".").join("/") + "/" + dependency.artifact
+      uri = "https://repo1.maven.org/maven2/" + dependency.group.split(".").join("/") + "/" + dependency.artifact
       if dependency.version
         uri += "/" + dependency.version
       end
