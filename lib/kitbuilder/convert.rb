266f97df3614aceff5b8c1584c92e82ace7dae49
Klaus Kämpf <kkaempf@gmail.com>
2024-08-02 15:51:13 +0200

    Fix the gradle conversion
    Copy *all* files, not only .pom
    
    (Ruby 'case' is different from C 'case' :facepalm:)
    
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- lib/kitbuilder/convert.rb
+++ lib/kitbuilder/convert.rb
@@ -13,27 +13,29 @@ module Kitbuilder
       @mavendir = mavendir
       Find.find(@gradledir) do |path|
         dest = nil
-        jars = []
+        files = []
         case path
+        # caches/modules-2/files-2.1/org.gradle.toolchains/foojay-resolver/0.8.0/4a3a39d3b507c3c93d04130c7246113428272cf3/foojay-resolver-0.8.0.pom
         when /\.pom$/
           pom = Pom.new path
+          # dir: two levels up
           dir = File.dirname(File.dirname(path))
-#          puts "#{dir}:#{pom.basename}"
-          # now look for jars or exes
+          # caches/modules-2/files-2.1/org.gradle.toolchains/foojay-resolver/0.8.0
+          puts "POM #{dir}:#{pom.basename}"
+          # now look for files below caches/modules-2/files-2.1/org.gradle.toolchains/foojay-resolver/0.8.0/...
           Find.find(dir) do |local|
             case local
-            when /\.jar$/
-            when /\.exe$/
-              jars << local
+            when /\.asc$/, /\.exe$/, /\.gz$/, /\.jar$/, /\.module$/, /\.zip$/
+              files << local
             end
           end
           dest = File.join(mavendir, pom.dirname)
           FileUtils.mkdir_p dest
           FileUtils.cp path, File.join(dest, "#{pom.basename}.pom")
-          jars.each do |jar|
-            name = File.basename(jar)
-#            puts "Dir #{dir}, jar #{jar}, name #{name}"
-            FileUtils.cp jar, File.join(dest, name)
+          files.each do |file|
+            name = File.basename(file)
+            puts "DIR #{dir}, file #{file}, name #{name}"
+            FileUtils.cp file, File.join(dest, name)
           end
         when /\.js$/
 #          puts path
